56ad6ff14d99d1cc7cb9a598dd1eea41b7d23e5e
Charles Boadu <charles.boadu@stu.ucc.edu.gh>
2024-04-02 01:55:27 +0000

    Add settings menu with icons and styling

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,9 +1,139 @@
-import { SafeAreaView, View, Text } from "react-native";
+import { SafeAreaView, View, Text, ScrollView } from "react-native";
+import tw from "tailwind-react-native-classnames";
+import RightArrow from "react-native-vector-icons/AntDesign";
+import AccountIcon from "react-native-vector-icons/MaterialCommunityIcons";
+import PrivacyIcon from "react-native-vector-icons/MaterialIcons";
+import SecurityIcon from "react-native-vector-icons/MaterialCommunityIcons";
+import NotificationsIcon from "react-native-vector-icons/Ionicons";
+import HelpIcon from "react-native-vector-icons/Entypo";
+import InformationIcon from "react-native-vector-icons/Feather";
 
 export const SettingsScreen = () => {
   return (
     <SafeAreaView>
-      <Text>Settings Screen</Text>
+      <View
+        style={[
+          tw`h-20 flex items-center justify-center`,
+          { backgroundColor: "#061551" },
+        ]}
+      >
+        <Text style={tw`text-white text-2xl font-bold`}>Settings</Text>
+      </View>
+      <View>
+        <Text style={tw`text-xl font-bold p-4`}>General</Text>
+      </View>
+      <ScrollView>
+        <View>
+          <View
+            style={[
+              tw`border-b m-4 py-2 flex flex-row justify-between`,
+              { borderColor: "gray" },
+            ]}
+          >
+            <View style={tw`flex flex-row`}>
+              <View
+                style={tw`h-8 w-8 rounded-lg bg-gray-200 mr-2 flex items-center justify-center`}
+              >
+                <AccountIcon name="account" size={20} style={tw``} />
+              </View>
+              <Text style={[tw`font-semibold pt-2`]}>Account</Text>
+            </View>
+            <View>
+              <RightArrow name="right" size={20} style={tw``} />
+            </View>
+          </View>
+          <View
+            style={[
+              tw`border-b m-4 py-2 flex flex-row justify-between`,
+              { borderColor: "gray" },
+            ]}
+          >
+            <View style={tw`flex flex-row`}>
+              <View
+                style={tw`h-8 w-8 rounded-lg bg-gray-200 mr-2 flex items-center justify-center`}
+              >
+                <PrivacyIcon name="privacy-tip" size={20} style={tw``} />
+              </View>
+              <Text style={[tw`font-semibold pt-2`]}>Privacy</Text>
+            </View>
+            <View>
+              <RightArrow name="right" size={20} style={tw``} />
+            </View>
+          </View>
+          <View
+            style={[
+              tw`border-b m-4 py-2 flex flex-row justify-between`,
+              { borderColor: "gray" },
+            ]}
+          >
+            <View style={tw`flex flex-row`}>
+              <View
+                style={tw`h-8 w-8 rounded-lg bg-gray-200 mr-2 flex items-center justify-center`}
+              >
+                <SecurityIcon name="security" size={20} style={tw``} />
+              </View>
+              <Text style={[tw`font-semibold pt-2`]}>Security</Text>
+            </View>
+            <View>
+              <RightArrow name="right" size={20} style={tw``} />
+            </View>
+          </View>
+          <View
+            style={[
+              tw`border-b m-4 py-2 flex flex-row justify-between`,
+              { borderColor: "gray" },
+            ]}
+          >
+            <View style={tw`flex flex-row`}>
+              <View
+                style={tw`h-8 w-8 rounded-lg bg-gray-200 mr-2 flex items-center justify-center`}
+              >
+                <NotificationsIcon name="notifications" size={20} style={tw``} />
+              </View>
+              <Text style={[tw`font-semibold pt-2`]}>Notification</Text>
+            </View>
+            <View>
+              <RightArrow name="right" size={20} style={tw``} />
+            </View>
+          </View>
+          <View
+            style={[
+              tw`border-b m-4 py-2 flex flex-row justify-between`,
+              { borderColor: "gray" },
+            ]}
+          >
+            <View style={tw`flex flex-row`}>
+              <View
+                style={tw`h-8 w-8 rounded-lg bg-gray-200 mr-2 flex items-center justify-center`}
+              >
+                <HelpIcon name="help" size={20} style={tw``} />
+              </View>
+              <Text style={[tw`font-semibold pt-2`]}>Help</Text>
+            </View>
+            <View>
+              <RightArrow name="right" size={20} style={tw``} />
+            </View>
+          </View>
+          <View
+            style={[
+              tw`border-b m-4 py-2 flex flex-row justify-between`,
+              { borderColor: "gray" },
+            ]}
+          >
+            <View style={tw`flex flex-row`}>
+              <View
+                style={tw`h-8 w-8 rounded-lg bg-gray-200 mr-2 flex items-center justify-center`}
+              >
+                <InformationIcon name="info" size={20} style={tw``} />
+              </View>
+              <Text style={[tw`font-semibold pt-2`]}>About</Text>
+            </View>
+            <View>
+              <RightArrow name="right" size={20} style={tw``} />
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
