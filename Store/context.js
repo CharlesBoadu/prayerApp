5f11693f08dc86c222d28dd1311387637c2a647f
Charles Boadu <charles.boadu@stu.ucc.edu.gh>
2024-04-06 17:12:30 +0000

    Add new dependencies and refactor prayer fetching logic

--- Store/context.js
+++ Store/context.js
@@ -9,80 +9,34 @@ export const PrayerAppProvider = ({ children }) => {
   const [showPrayers, setShowPrayers] = useState(false);
   const [favoritesCount, setFavoritesCount] = useState(0);
   const [favoritePrayers, setFavoritePrayers] = useState([]);
-  const [fetchedHealthPrayers, setFetchedHealthPrayers] = useState([]);
-  const [fetchedWealthPrayers, setFetchedWealthPrayers] = useState([]);
-  const [fetchedWarfarePrayers, setFetchedWarfarePrayers] = useState([]);
-  const [fetchedPraisePrayers, setFetchedPraisePrayers] = useState([]);
-  const [fetchedProtectionPrayers, setFetchedProtectionPrayers] = useState([]);
-  const [selectedPrayerCategory, setSelectedPrayerCategory] = useState("dfdwf");
+  const [fetchedPrayers, setFetchedPrayers] = useState([]);
+  const [selectedPrayerCategory, setSelectedPrayerCategory] = useState("");
   const [newNotification, setNewNotification] = useState(true);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchHealthPrayers = async () => {
+    const fetchPrayers = async () => {
       try {
-        const response = await fetch("http://172.20.10.13:6000/health/all");
+        const response = await fetch("http://127.0.0.1:5000/api/v1/prayers");
         const data = await response.json();
-        setFetchedHealthPrayers(data?.data);
+        setFetchedPrayers(data?.data);
+        setTimeout(() => {
+          setLoading(false);
+        }, 2000);
       } catch (error) {
-        console.error("Error fetching health prayers:", error);
+        console.error("Error Fetching All Prayers:", error);
       }
     };
 
-    const fetchWealthPrayers = async () => {
-      try {
-        const response = await fetch("http://172.20.10.13:6000/wealth/all");
-        const data = await response.json();
-        setFetchedWealthPrayers(data?.data);
-      } catch (error) {
-        console.error("Error fetching wealth prayers:", error);
-      }
-    };
-
-    const fetchWarfarePrayers = async () => {
-      try {
-        const response = await fetch("http://172.20.10.13:6000/warfare/all");
-        const data = await response.json();
-        setFetchedWarfarePrayers(data?.data);
-      } catch (error) {
-        console.error("Error fetching warfare prayers:", error);
-      }
-    };
-
-    const fetchProtectionPrayers = async () => {
-      try {
-        const response = await fetch("http://172.20.10.13:6000/protection/all");
-        const data = await response.json();
-        setFetchedProtectionPrayers(data?.data);
-      } catch (error) {
-        console.error("Error fetching protection prayers:", error);
-      }
-    };
-
-    const fetchPraisePrayers = async () => {
-      try {
-        const response = await fetch("http://172.20.10.13:6000/praise/all");
-        const data = await response.json();
-        setFetchedPraisePrayers(data?.data);
-      } catch (error) {
-        console.log("Error fetching praise prayers:", error);
-      }
-    };
-
-    // fetchHealthPrayers();
-    // fetchWealthPrayers();
-    // fetchWarfarePrayers();
-    // fetchProtectionPrayers();
-    // fetchPraisePrayers();
+    fetchPrayers();
   }, []);
 
   return (
     <PrayerAppContext.Provider
       value={{
-        fetchedHealthPrayers,
-        fetchedWealthPrayers,
-        fetchedWarfarePrayers,
-        fetchedPraisePrayers,
-        fetchedProtectionPrayers,
+        loading,
+        setLoading,
+        fetchedPrayers,
         isAuthenticated,
         setIsAuthenticated,
         globalName,
--- screens/PrayerCategoryScreen.js
+++ screens/PrayerCategoryScreen.js
@@ -1,48 +1,123 @@
-import { SafeAreaView, View, Text, TouchableOpacity } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity,
+  ActivityIndicator,
+  Image,
+  StyleSheet,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { usePrayerAppContext } from "../Store/context";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { BackButton } from "../components/BackButton";
+import { FlatList } from "react-native-gesture-handler";
 import tw from "tailwind-react-native-classnames";
+import Spinner from "react-native-loading-spinner-overlay";
+// import LoadingSpinner from 'react-native-spinkit';
 
 export const PrayerCategoryScreen = () => {
   const navigation = useNavigation();
-  const { selectedPrayerCategory, setSelectedPrayerCategory } = usePrayerAppContext();
+  const {
+    selectedPrayerCategory,
+    setSelectedPrayerCategory,
+    fetchedPrayers,
+    loading,
+  } = usePrayerAppContext();
+
+  // console.log("Fetched Prayers", fetchedPrayers);
+  // console.log("loading", loading);
 
   useEffect(() => {
     const getSelectedCategory = async () => {
       const category = await AsyncStorage.getItem("selectedPrayerCategory");
       setSelectedPrayerCategory(category);
-    }
+    };
+
+    // const categorizePrayers = () => {
+    //   const category = fetchedPrayers.map((prayer) => prayer.category);
+    //   if (category.toLowerCase() === "health") {
+    //     setHealthPrayers(category);
+    //   } else if (category.toLowerCase() === "wealth") {
+    //     setPraisePrayers(category);
+    //   } else if (category.toLowerCase() === "warfare") {
+    //     setWarfarePrayers(category);
+    //   } else if (category.toLowerCase() === "praise") {
+    //     setPraisePrayers(category);
+    //   } else if (category.toLowerCase() === "protection") {
+    //     setProtectionPrayers(category);
+    //   }
+    // };
 
     getSelectedCategory();
-  }, [])
+    // categorizePrayers();
+  }, []);
   return (
-    <SafeAreaView>
+    <SafeAreaView style={tw`bg-white h-full`}>
       <BackButton />
-      {/* <FlatList
-        data={prayerCategories}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <TouchableOpacity
-            style={styles.item}
-            onPress={() =>
-              navigation.navigate("PrayerList", { category: item })
-            }
-          >
-            <Text style={styles.title}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      /> */}
       <View
         style={[
           tw`h-14 flex items-center justify-center`,
           { backgroundColor: "#061551" },
         ]}
       >
-        <Text style={tw`text-white text-2xl font-bold`}>Prayers About {selectedPrayerCategory} </Text>
+        <Text style={tw`text-white text-2xl font-bold`}>
+          Prayers About {selectedPrayerCategory}{" "}
+        </Text>
       </View>
+      {loading ? (
+        <View style={tw`flex items-center justify-center h-60`}>
+          <ActivityIndicator size="large" color="#061551" />
+        </View>
+      ) : (
+        <>
+          {fetchedPrayers.some(
+            (item) =>
+              item.category.toLowerCase() ===
+              selectedPrayerCategory.toLowerCase()
+          ) ? (
+            <FlatList
+              data={fetchedPrayers.filter(
+                (prayer) =>
+                  prayer?.category.toLowerCase() ===
+                  selectedPrayerCategory.toLowerCase()
+              )}
+              keyExtractor={(item) => item.user_id.toString()}
+              renderItem={({ item }) => (
+                (
+                  <TouchableOpacity
+                    // style={styles.item}
+                    onPress={() =>
+                      navigation.navigate("PrayerList", { category: item })
+                    }
+                  >
+                    <Text>{item.prayer}</Text>
+                  </TouchableOpacity>
+                )
+              )}
+            />
+          ) : (
+            <View style={tw`flex items-center justify-center`}>
+              <Image
+                source={require("../assets/no-data.jpg")}
+                alt="No Prayers Image"
+                style={styles.image}
+              />
+              <Text style={tw`text-xl font-bold`}>
+                No Prayers About {selectedPrayerCategory} Yet
+              </Text>
+            </View>
+          )}
+        </>
+      )}
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    width: "70%",
+    height: 300,
+  },
+});
